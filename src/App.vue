c81ce1504e71769864e58a3a5125523d48d75349
roascua <ro.ascua24@gmail.com>
2026-06-09 19:47:54 -0300

    Proyecto inicial Vue + Tailwind

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-slate-100">
+    <h1 class="text-5xl font-bold text-blue-600">
+      Tienda Saphirus
+    </h1>
+  </div>
+</template>
